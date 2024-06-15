37721131ee502e8a05de53cc46452be0786b1180
ashutosh <mr.luckysharma7@gmail.com>
2024-06-15 15:22:36 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import EnergyChart from "./components/dashboard";
+
+function App() {
+  const [count, setCount] = useState([]);
+  const [filter, setfilter] = useState("All");
+  const [filtereddata, setfiltereddata] = useState(count);
+
+  const [pageperdata, setpageperdata] = useState(100);
+
+  useEffect(() => {
+    const data = async () => {
+      const datasize = await fetch("https://online-1-5nf6.onrender.com/data/dashboard", {
+        cache: "force-cache",
+      }).then((res) => res.json());
+      setCount(datasize.data.data);
+    };
+    data();
+  }, []);
+  useEffect(() => {
+    setfiltereddata(count);
+  }, [count]);
+
+  useEffect(() => {
+    console.log(count)
+    setfiltereddata(count?.data?.filter((res) => res.sector === filter));
+  }, [filter]);
+
+  // const sectors = count.data.reduce((item , daa)=>{if(!daa.includes(item){
+  //   daa.push(item)
+  // })})
+
+  // console.log(count?.data?.filter((res) => res.sector === "Energy"));
+
+  return (
+    <>
+      <select
+        value={filter}
+        onChange={(e) => {
+          setfilter(e.target.value);
+        }}
+      >
+        <option value="">all</option>
+        <option value="Energy">energy</option>
+      </select>
+      <select
+        value={pageperdata}
+        onChange={(e) => {
+          setpageperdata(e.target.value);
+        }}
+      >
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="30">30</option>
+        <option value="100">all</option>
+      </select>
+      <EnergyChart data={filtereddata.data?.slice(0, pageperdata)} />;
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/dashboard.tsx
@@ -0,0 +1,47 @@
+import { Bar } from "react-chartjs-2";
+import "chart.js/auto";
+
+const EnergyChart = ({ data }) => {
+  // Extract data for the chart
+  const labels = data?.map((item) =>
+    new Date(item.published).toLocaleDateString()
+  );
+  const intensities = data?.map((item) => item.intensity);
+  const relevance = data?.map((item) => item.relevance);
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Intensity",
+        data: intensities,
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "relevance",
+        data: relevance,
+        backgroundColor: "rgba(750, 192, 192, 0.2)",
+        borderColor: "rgba(751, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div style={{ width: "80vw", height: "1200px", margin: "auto" }}>
+      <Bar data={chartData} options={options} />
+    </div>
+  );
+};
+
+export default EnergyChart;
